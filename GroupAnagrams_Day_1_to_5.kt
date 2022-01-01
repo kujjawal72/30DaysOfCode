a5a2e4f054353462f31c34b070a8180122cb8233
Ujjawal Kumar <96038205+kujjawal72@users.noreply.github.com>
2022-01-01 15:19:27 +0530

    Create GroupAnagrams_Day_1_to_5.kt

---
+++ GroupAnagrams_Day_1_to_5.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val mainMap = HashMap<HashMap<Char,Int>,ArrayList<String>>();
+        strs.forEach{it: String->
+            //val temp = it.toCharArray().sorted().joinToString(separator = "")
+            val map = HashMap<Char,Int>()
+            it.toCharArray().forEach{c->
+                val x = map.get(c)
+                map.put(c,if(x == null) 1 else x+1)
+            }
+            val x = mainMap.get(map);
+            //print(map +" " + x)
+            if(x == null){
+                mainMap.put(map,arrayListOf(it))
+            }else{
+                x!!.add(it)
+                mainMap.put(map,x!!);
+            }
+        }
+        return mainMap.values.toList();
+    }
+}
